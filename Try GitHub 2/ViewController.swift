5f5b555c87a3925ad3e81e5361043772192a18b0
Your Name <oz9080@gmail.com>
2018-02-13 09:57:38 +0200

    Changes

---
+++ Try GitHub 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Try GitHub 2
+//
+//  Created by Oz Arie Tal Shachar on 13.2.2018.
+//  Copyright © 2018 Oz Arie Tal Shachar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
